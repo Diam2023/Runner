102df476f91585fb7732da947459837969aef6a7
monoliths <monoliths-uni@outlook.com>
2023-07-08 18:00:04 +0800

    Start scene change to MenuScene

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -23,7 +23,8 @@
  ****************************************************************************/
 
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
+
+#include "MenuScene.h"
 
 // #define USE_AUDIO_ENGINE 1
 
@@ -108,7 +109,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+    auto scene = MenuScene::createScene();
 
     // run
     director->runWithScene(scene);
