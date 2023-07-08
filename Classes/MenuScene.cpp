8ff6bd8adde5d80f5c9db50a60600371b798e081
monoliths <monoliths-uni@outlook.com>
2023-07-08 16:45:08 +0800

    Change Source File Search Method

---
+++ Classes/MenuScene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 35691 on 7/8/2023.
+//
+
+#include "MenuScene.h"
---
+++ Classes/MenuScene.h
@@ -0,0 +1,16 @@
+//
+// Created by 35691 on 7/8/2023.
+//
+
+#ifndef RUNNER_MENUSCENE_H
+#define RUNNER_MENUSCENE_H
+
+
+
+class MenuScene {
+
+};
+
+
+
+#endif //RUNNER_MENUSCENE_H
