94c0681f6ed9d4c9a0ba77df96826769bc926fa7
monoliths <monoliths-uni@outlook.com>
2023-07-10 00:52:19 +0800

    Formateed Code With Clang-Format

--- Classes/AppDelegate.h
+++ Classes/AppDelegate.h
@@ -36,28 +36,28 @@ class AppDelegate : private cocos2d::Application
 {
   public:
     AppDelegate();
-    virtual ~AppDelegate();
+    ~AppDelegate() override;
 
-    virtual void initGLContextAttrs();
+    void initGLContextAttrs() override;
 
     /**
     @brief    Implement Director and Scene init code here.
     @return true    Initialize success, app continue.
     @return false   Initialize failed, app terminate.
     */
-    virtual bool applicationDidFinishLaunching();
+    bool applicationDidFinishLaunching() override;
 
     /**
     @brief  Called when the application moves to the background
     @param  the pointer of the application
     */
-    virtual void applicationDidEnterBackground();
+    void applicationDidEnterBackground() override;
 
     /**
     @brief  Called when the application reenters the foreground
     @param  the pointer of the application
     */
-    virtual void applicationWillEnterForeground();
+    void applicationWillEnterForeground() override;
 };
 
 #endif  // _APP_DELEGATE_H_
