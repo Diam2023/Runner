f3cd082364ce1f7645717e73738e5ac951c157bf
monoliths <monoliths-uni@outlook.com>
2023-07-10 00:51:00 +0800

    Formateed Code With Clang-Format

---
+++ Classes/GameScene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 35691 on 7/10/2023.
+//
+
+#include "GameScene.h"
---
+++ Classes/GameScene.h
@@ -0,0 +1,16 @@
+//
+// Created by 35691 on 7/10/2023.
+//
+
+#ifndef RUNNER_GAMESCENE_H
+#define RUNNER_GAMESCENE_H
+
+
+
+class GameScene {
+
+};
+
+
+
+#endif //RUNNER_GAMESCENE_H
