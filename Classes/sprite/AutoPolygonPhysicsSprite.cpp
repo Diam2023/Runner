a22cc33cef645132b30ad69d1ceebf7f8b84cb79
monoliths <monoliths-uni@outlook.com>
2023-07-19 20:12:26 +0800

    Create AutoPolygonPhysicsSprite For Project DriveSprite class

--- Classes/sprite/AutoPolygonPhysicsSprite.cpp
+++ Classes/sprite/AutoPolygonPhysicsSprite.cpp
@@ -3,3 +3,102 @@
 //
 
 #include "AutoPolygonPhysicsSprite.h"
+
+#include "utils/ImageAutoPolygon.h"
+#include "renderer/CCTexture2D.h"
+
+USING_NS_CC;
+
+cocos2d::PhysicsBody* AutoPolygonPhysicsSprite::generatePhysicsBody(
+    cocos2d::Image* image,
+    const cocos2d::Size& contentSize,
+    const cocos2d::PhysicsMaterial& physicsMaterial)
+{
+    // TODO Rewrite This Code For Support PolygonSprite Bug
+    // First Thing Is Initialize With Info
+
+    const auto polygonInfo = ImageAutoPolygon::generatePolygon(image);
+
+    auto triangles = polygonInfo.triangles;
+    auto points = new Vec2[triangles.indexCount];
+    for (uint32_t i = 0; i < triangles.indexCount; ++i)
+    {
+        points[i].x = triangles.verts[triangles.indices[i]].vertices.x -
+                      contentSize.width / 2;
+        points[i].y = triangles.verts[triangles.indices[i]].vertices.y -
+                      contentSize.height / 2;
+    }
+
+    // Create A PhysicsBody
+    auto physicsBody =
+        PhysicsBody::createPolygon(points,
+                                   static_cast<int>(triangles.indexCount),
+                                   physicsMaterial);
+    CCASSERT(physicsBody == nullptr,
+             "Null Sprite Object of Create New Image Callback!!!");
+    physicsBody->autorelease();
+    CC_SAFE_DELETE_ARRAY(points);
+    return physicsBody;
+}
+
+AutoPolygonPhysicsSprite::AutoPolygonPhysicsSprite() : roleMaterial()
+{
+    CCLOGINFO("AutoPolygonPhysicsSprite Create!");
+}
+
+AutoPolygonPhysicsSprite::~AutoPolygonPhysicsSprite()
+{
+    CCLOGINFO("AutoPolygonPhysicsSprite Destroy!");
+}
+AutoPolygonPhysicsSprite* AutoPolygonPhysicsSprite::create(
+    cocos2d::Texture2D* texture,
+    cocos2d::PhysicsMaterial physicsMaterial)
+{
+    auto tmpSprite = new (std::nothrow) Sprite();
+    if (!tmpSprite->initWithTexture(texture))
+    {
+        CCLOGERROR("Error Initialize TmpSprite!!!");
+        return nullptr;
+    }
+
+    tmpSprite->setAnchorPoint(Vec2::ZERO);
+    tmpSprite->autorelease();
+
+    // Create renderTexture
+    RenderTexture* tmpRender = RenderTexture::create(
+        static_cast<int>(tmpSprite->getContentSize().width),
+        static_cast<int>(tmpSprite->getContentSize().height),
+        backend::PixelFormat::RGBA8888);
+    tmpRender->autorelease();
+    tmpRender->setSprite(tmpSprite);
+    tmpRender->begin();
+    //! TODO Do not use This in Debug Draw Sprite
+    tmpSprite->visit();
+    tmpRender->end();
+
+    auto sprite = new AutoPolygonPhysicsSprite();
+    CCASSERT(sprite == nullptr,
+             "Create AutoPolygonPhysicsSprite Error!!!");
+    sprite->autorelease();
+    sprite->initWithTexture(texture);
+    sprite->setRoleMaterial(physicsMaterial);
+
+    tmpRender->createNewImage([sprite](Image* image) {
+        CCASSERT(image == nullptr,
+                 "Null Image Object of Create New Image Callback!!!");
+        CCASSERT(sprite == nullptr,
+                 "Null Sprite Object of Create New Image Callback!!!");
+
+        auto physicsBody = AutoPolygonPhysicsSprite::generatePhysicsBody(
+            image, sprite->getContentSize(), sprite->getRoleMaterial());
+
+        sprite->setPhysicsBody(physicsBody);
+        sprite->setVisible(true);
+
+        CC_SAFE_DELETE(image);
+    });
+
+    sprite->setVisible(false);
+
+    return sprite;
+}
--- Classes/sprite/AutoPolygonPhysicsSprite.h
+++ Classes/sprite/AutoPolygonPhysicsSprite.h
@@ -2,15 +2,51 @@
 // Created by 35691 on 7/19/2023.
 //
 
-#ifndef RUNNER_AUTOPOLYGONPHYSICSSPRITE_H
-#define RUNNER_AUTOPOLYGONPHYSICSSPRITE_H
+#pragma once
 
+#include "cocos2d.h"
 
+class AutoPolygonPhysicsSprite : public cocos2d::Sprite
+{
+  protected:
+    /**
+     * Sprite Material
+     */
+    cocos2d::PhysicsMaterial roleMaterial;
 
-class AutoPolygonPhysicsSprite {
+  public:
+    const cocos2d::PhysicsMaterial& getRoleMaterial() const
+    {
+        return roleMaterial;
+    }
+    void setRoleMaterial(const cocos2d::PhysicsMaterial& roleMaterial_)
+    {
+        AutoPolygonPhysicsSprite::roleMaterial = roleMaterial_;
+    }
 
+  public:
+
+    /**
+     * constructor
+     */
+    AutoPolygonPhysicsSprite();
+    ~AutoPolygonPhysicsSprite() override;
+
+    /**
+     * Create A AutoPolygonPhysicsSprite With texture and physicsMaterial
+     * @param texture texture of Sprite
+     * @param physicsMaterial physicsMaterial For PhysicsBody
+     * @return A new Auto Relase AutoPolygonPhysicsSprite Instance
+     */
+    static AutoPolygonPhysicsSprite* create(cocos2d::Texture2D *texture, cocos2d::PhysicsMaterial physicsMaterial = cocos2d::PHYSICSBODY_MATERIAL_DEFAULT);
+
+    /**
+     * Create A PhysicsBody With Auto Split Polygon
+     * @param image image
+     * @param contentSize size of sprite
+     * @param physicsMaterial material
+     * @return A auto release PhysicsBody Instance
+     */
+    static cocos2d::PhysicsBody* generatePhysicsBody(cocos2d::Image* image, const cocos2d::Size& contentSize, const cocos2d::PhysicsMaterial& physicsMaterial);
 };
 
-
-
-#endif //RUNNER_AUTOPOLYGONPHYSICSSPRITE_H
